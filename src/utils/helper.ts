1be0c00f6e909045fe1ca43ad38b8c7af5d8c757
Babatunde Ojo <babatundeojo30@gmail.com>
2021-12-16 14:03:17 +0100

    add more text

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -16,11 +16,24 @@ export const generateSentence  = () => {
   const rand5 = Math.floor(Math.random() * 10);
   const rand6 = Math.floor(Math.random() * 10);
 
-  const content = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + 
+  const rand7 = Math.floor(Math.random() * 10);
+  const rand8 = Math.floor(Math.random() * 10);
+  const rand9 = Math.floor(Math.random() * 10);
+  const rand10 = Math.floor(Math.random() * 10);
+  const rand11 = Math.floor(Math.random() * 10);
+  const rand12 = Math.floor(Math.random() * 10);
+
+  const content1 = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + 
     adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + 
     " " + adverbs[rand1] + " " + verbs[rand1] + " " + preposition[rand1] + " a " + 
     adjectives[rand2] + " " + nouns[rand5] + " which, became a " + adjectives[rand3] + 
     ", " + adjectives[rand4] + " " + nouns[rand6] + ".";
 
-  return content;
+  const content2= "The " + adjectives[rand7] + " " + nouns[rand8] + " " + 
+    adverbs[rand9] + " " + verbs[rand10] + " because some " + nouns[rand7] + 
+    " " + adverbs[rand7] + " " + verbs[rand7] + " " + preposition[rand7] + " a " + 
+    adjectives[rand8] + " " + nouns[rand11] + " which, became a " + adjectives[rand9] + 
+    ", " + adjectives[rand10] + " " + nouns[rand12] + ".";
+
+  return `${content1} ${content2}`;
 }
